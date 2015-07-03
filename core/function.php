97f0c980847fb6580fa6e816cbb214588ee41ea4
Isaac de Cuba <i.de.cuba@youwe.nl>
2015-07-03 11:28:47 +0200

    cor js

--- core/function.php
+++ core/function.php
@@ -1,26 +1,24 @@
 <?php
 
-function get_omschrijving() 
+function get_omschrijving()
 {
- 	 $html = "";
-	$url = 'http://www.veiligstallen.nl/veiligstallen.xml';
-		$xml = simpleXML_load_file($url,"SimpleXmlElement");
-?>
-		<select name="years">
+    $html = "";
+    $url = 'http://www.veiligstallen.nl/veiligstallen.xml';
+    $xml = simpleXML_load_file($url, "SimpleXmlElement");
+    ?>
+    <select name="years">
+        <?php
+        foreach ($xml->Fietsenstalling as $oms) {
+
+            echo "<option ><strong>Gemeente:</strong> " . $oms->Gemeente . "</option><br/>";
+
+
+            echo "</p>";
+        }
+        ?>
+    </select>
 <?php
-		foreach ($xml->Fietsenstalling as $oms) 
-		{
-
-			echo "<option ><strong>Gemeente:</strong> ".$oms->Gemeente."</option><br/>";
-
-
-			echo "</p>";
-		}
-		?>
-		</select>
-		<?php
 }
-
 
 
 ?>
