e41a042b3c18b4a5779bc70e8e5ed0e229b0f5ef
isaac youwe <isaac-youwe@gmail.com>
2015-07-07 23:49:46 +0200

    added search form

--- index.php
+++ index.php
@@ -16,6 +16,10 @@ $stallingen = $call->getStallingen();
 
     <div class="row ">
         <div class="col-md-2 col-sm-12 ">
+            <div class="input-group">
+                <span class="input-group-addon" id="basic-addon1">@</span>
+                <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
+            </div>
         </div>
         <div class="col-md-10 col-sm-12">
             <?php include 'includes/map.php' ?>
