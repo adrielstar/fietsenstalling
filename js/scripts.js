75a9b1821b9db24b16541261d85811b4f60e341d
adrielstar <adri_star1986@hotmail.com>
2015-07-07 14:03:14 +0200

    your location

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,110 @@
-function initialise() {
+var map;
+
+function initialize() {
+
+    var markers = [];
     myLatlng = new google.maps.LatLng(52, 4);
     mapOptions = {
         zoom: 8,
         center: myLatlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-    }
-    geocoder = new google.maps.Geocoder();
-    infoWindow = new google.maps.InfoWindow;
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-     
-    xmlUrl = "veiligstallen.xml";
-     //request
-    //loadMarkers();
-     
-}
- 
-google.maps.event.addDomListener(window, 'load', initialise); 
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
 
-function loadMarkers() {
-    map.markers = map.markers || []
-    downloadUrl(xmlUrl, function(data) {
-        var xml = data.responseXML;
-        markers = xml.documentElement.getElementsByTagName("Fietsenstalling");
-        for (var i = 0; i < markers.length; i++) {
-            var coordinaten = markers[i].getAttribute("Coordinaten");
-            
-            var id = markers[i].getAttribute("ID");
-            var address = markers[i].getAttribute("address1")+"<br />"+markers[i].getAttribute("address2")+"<br />"+markers[i].getAttribute("Straat")+"<br />"+markers[i].getAttribute("Postcode");
-           
-            var point = new google.maps.LatLng(
-                parseFloat(markers[i].getAttribute("lat")),
-                parseFloat(markers[i].getAttribute("lng")));
-            var html = "<div class='infowindow'><b>" + name + "</b> <br/>" + address+'<br/></div>';
-            var marker = new google.maps.Marker({
-              map: map,
-              position: point,
-              title: name
+    // Try HTML5 geolocation
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            pos = new google.maps.LatLng(position.coords.latitude,
+                position.coords.longitude);
+
+            infowindow = new google.maps.InfoWindow({
+                map: map,
+                position: pos,
+                content: 'Location found.'
             });
-            map.markers.push(marker);
-            bindInfoWindow(marker, map, infoWindow, html);
+
+            map.setCenter(pos);
+        }, function () {
+            handleNoGeolocation(true);
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleNoGeolocation(false);
+    }
+
+    // Create the search box and link it to the UI element.
+    input = /** @type {HTMLInputElement} */(
+        document.getElementById('search-input'));
+
+
+    searchBox = new google.maps.places.SearchBox(
+        /** @type {HTMLInputElement} */(input));
+    // Listen for the event fired when the user selects an item from the
+    // pick list. Retrieve the matching places for that item.
+    google.maps.event.addListener(searchBox, 'places_changed', function () {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
         }
+        for (var i = 0, marker; marker = markers[i]; i++) {
+            marker.setMap(null);
+        }
+
+        // For each place, get the icon, place name, and location.
+        markers = [];
+        var bounds = new google.maps.LatLngBounds();
+        for (var i = 0, place; place = places[i]; i++) {
+            var image = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            var marker = new google.maps.Marker({
+                map: map,
+                icon: image,
+                title: place.name,
+                zoom: 2,
+                position: place.geometry.location
+            });
+
+            markers.push(marker);
+
+            bounds.extend(place.geometry.location);
+        }
+
+        map.fitBounds(bounds);
     });
+
+    // Bias the SearchBox results towards places that are within the bounds of the
+    // current map's viewport.
+    google.maps.event.addListener(map, 'bounds_changed', function () {
+        var bounds = map.getBounds();
+        searchBox.setBounds(bounds);
+    });
+
 }
+
+function handleNoGeolocation(errorFlag) {
+    if (errorFlag) {
+        var content = 'Error: The Geolocation service failed.';
+    } else {
+        var content = 'Error: Your browser doesn\'t support geolocation.';
+    }
+
+    options = {
+        map: map,
+        position: new google.maps.LatLng(60, 105),
+        content: content
+    };
+
+    infowindow = new google.maps.InfoWindow(options);
+    map.setCenter(options.position);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
